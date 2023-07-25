f97fd5777cfd67f5ad12440831ca832a6c3f5f3a
Diego <dportillodeveloper@gmail.com>
2023-07-25 14:05:49 -0400

    Reto 1

--- src/ui/TodoSearch/index.js
+++ src/ui/TodoSearch/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import './TodoSearch.css';
+import './TodoSearch.css'
+import { useSearchParams } from 'react-router-dom'
 
-function TodoSearch({ searchValue, setSearchValue, loading }) {
-  const onSearchValueChange = (event) => {
-    console.log(event.target.value);
-    setSearchValue(event.target.value);
-  };
+function TodoSearch({ setSearchValue, loading }) {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const paramsValue = searchParams.get('search')
 
-  return (
-    <input
-      className="TodoSearch"
-      placeholder="Cebolla"
-      value={searchValue}
-      onChange={onSearchValueChange}
-      disabled={loading}
-    />
-  );
+    const onSearchValueChange = ({ target: { value } }) => {
+        setSearchValue(value)
+        setSearchParams({ search: value })
+    }
+
+    return (
+        <input
+            className={`TodoSearch ${loading && 'TodoSearch--loading'}`}
+            onChange={onSearchValueChange}
+            value={paramsValue ?? ''}
+            placeholder="Search a To-Do"
+        />
+    )
 }
 
 export { TodoSearch };
